6c94c0616562ee5483a1958d818466be9b756b1e
Matthias Hertel <me@matthiashertel.eu>
2016-12-29 09:41:20 +0100

    add hauptmenue button to manualview

--- dev/Spaceinvaders/src/org/blueberry/spaceinvaders/controller/ManualViewController.java
+++ dev/Spaceinvaders/src/org/blueberry/spaceinvaders/controller/ManualViewController.java
@@ -1,9 +1,11 @@
 package org.blueberry.spaceinvaders.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import org.blueberry.spaceinvaders.SpaceInvaders;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,5 +21,10 @@ public class ManualViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         WebEngine engine = manualView.getEngine();
        engine.load(getClass().getResource("/manual/manual.html").toExternalForm());
+    }
+
+    @FXML
+    private void goToScreen1(ActionEvent event){
+        SpaceInvaders.setScreen("WelcomeView");
     }
 }
