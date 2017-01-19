e72ddea81dc733065971a7646494fea410c34814
Matthias Hertel <me@matthiashertel.eu>
2017-01-19 20:27:15 +0100

    tidy up

--- dev/Spaceinvaders/src/org/blueberry/spaceinvaders/chat/ChatMessageListViewCell.java
+++ dev/Spaceinvaders/src/org/blueberry/spaceinvaders/chat/ChatMessageListViewCell.java
@@ -7,6 +7,9 @@ import javafx.scene.control.ListCell;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import org.apache.commons.lang.StringEscapeUtils;
 import java.text.SimpleDateFormat;
 
@@ -66,14 +69,17 @@ public class ChatMessageListViewCell extends ListCell<MessageTypeMessage>{
 				color = "black";
 			}
 
-			label1.setStyle("-fx-text-fill:" + color);
+			label1.setStyle("-fx-text-fill:" + color + "; -fx-font-weight: bold;");
+            label1.setFont(Font.font("Verdana", FontPosture.REGULAR, 12));
 			SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
 			long unixTimestamp = new Long(message.getTime());
 			String strtime = formatter.format(unixTimestamp);
 			label3.setText(strtime);
+            label3.setFont(Font.font("Verdana", FontPosture.REGULAR, 12));
 
 			String afterDecoding = StringEscapeUtils.unescapeHtml(message.getText());
 			label5.setText(afterDecoding);
+            label5.setFont(Font.font("Verdana", FontPosture.ITALIC, 12));
 
 
 
@@ -84,44 +90,3 @@ public class ChatMessageListViewCell extends ListCell<MessageTypeMessage>{
 	}
 
 }
-
-
-//chatListView.setCellFactory(list -> {
-//		ListCell<MessageTypeMessage> cell = new ListCell<MessageTypeMessage>() {
-//@Override
-//protected void updateItem(MessageTypeMessage item, boolean empty) {
-////                                    System.out.println(item);
-//		super.updateItem(item, empty);
-//
-//		setText(empty ? null : item.toChatString());
-////                                    setText();
-////                                    List<String> message = new ArrayList<String>();
-////                                    // this.setStyle("-fx-text-fill: " + color);
-////                                    message.add(item.getAuthor());
-////                                    // this.setStyle("-fx-text-fill: black");
-////                                    message.add(item.getTime());
-////                                    message.add(item.getText());
-////                                    for (int i=0; i<message.size(); i++) {
-////                                        Label lbl = new Label(message.get(i));
-////                                        lbl.setStyle("-fx-text-fill: " + item.getColor());
-////                                        setText(lbl.toString());
-////                                    }
-////                                    // nur getAuthor soll textfill bekommen
-////                                    System.out.println(item.getAuthor());
-////                                    // black
-////                                    System.out.println(item.getTime());
-////                                    System.out.println(item.getText());
-//
-//		if (!isEmpty()){
-//
-//		String color = item.getColor();
-//		if (color == null || color.isEmpty() || color.equals("null")) {
-//		color = "black";
-//		}
-//		setStyle("-fx-text-fill: " + color);
-//
-//		}
-//		}
-//		};
-//		return cell;
-//		});
